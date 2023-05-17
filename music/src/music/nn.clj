e644a77a1cc9dc711627b14fe51ca31b1b5f856e
Sarah Park <shpark23@amherst.edu>
2023-05-16 22:20:10 -0400

    edits

--- music/src/music/nn.clj
+++ music/src/music/nn.clj
@@ -5,6 +5,7 @@
   (:import (java.util Random)))
 
 
+;; PYTHON:
 ;; model = Sequential ()
 ;; model.add (Dense (512, activation='relu', input_dim=3))
 ;; model.add (Dense (512, activation='relu'))
@@ -29,15 +30,35 @@
 
 ;; How do I use leaky-re-lu?
 ;; What should the weights be? 
-(def network
-  (net/json->
-    "[[{\"activationF\" : \"leaky-re-lu\", \"weights\" : [-0.5,0.1,0.8]},
-       {\"activationF\" : \"leaky-re-lu\", \"weights\" : [0.7,0.6,-0.1]},
-      [{\"activationF\" : \"sigmoid\", \"weights\" : [0.5,-0.3,-0.4,-0.5]}]]"))
+
+(net/->net
+    [(inputNum (readBachDataset)) (- (inputNum (readBachDataset)) (/ (inputNum (readBachDataset)) 2)) 3]
+    (fn [_] fun/leaky-re-lu)
+    (fn [_] (rand)))
+
+;; (def network
+;;   (net/json->
+;;     "[[{\"activationF\" : \"leaky-re-lu\", \"weights\" : [-0.5,0.1,0.8]},
+;;        {\"activationF\" : \"leaky-re-lu\", \"weights\" : [0.7,0.6,-0.1]},
+;;       [{\"activationF\" : \"sigmoid\", \"weights\" : [0.5,-0.3,-0.4,-0.5]}]]"))
+
+(reduce
+ (fn [acc [xs ys]]
+   (net/fit acc 0.1 xs ys))
+ net
+ [[[0.2 0.6] [0.9]]
+  [[0.1 0.8] [0.2]]
+  [[0.5 0.4] [0.6]]])
+
+;; (net/fit
+;;  net
+;;  0.1
+;;  [0.2 0.6]
+;;  [0.9])
 
 (net/->svg
  custom-network)
 
 (net/predict
   network
-  [0.2 0.6])
+  [0.2 0.6]) ;;has to predict from a melody we feed it into
